e950f2f4fae5804e0433c67d79fcbfe661216e11
hcheng826 <lilioo826@gmail.com>
2023-03-16 00:15:45 +0800

    rename and re-structure

--- src/LoanAgent.sol
+++ src/LoanAgent.sol
@@ -2,34 +2,61 @@
 pragma solidity ^0.8.13;
 
 contract LoadAgent {
-    address public minerAddress;
-    address public lendingVaultAddress;
-    address public oracleAddress;
-
-    enum LoadAgentStatus {
-        // Stake Pool is in the collateral queue
-        InQueue,
-        // Stake pool is waiting for the node to transfer the onwerhsip address to StakePool
-        // with updated quota and epoch
-        OwnershipConfirmationPending,
-        // Stake Pool is waiting for the node to host an oracle and confirm its liveliness by responding to a challenge
-        OracleConfirmationPending,
-        // Stake Pool is active
-        Active,
-        // Stake Pool is paused
-        Paused,
-        // Stake Pool is terminated
-        Terminated,
-        // Stake pool is active but the miner is in the queue to add more power
-        AsyncInQueue,
+    struct AddressInfo {
+        address miner;
+        address realOwner;
+        address oracle;
     }
+
+    AddressInfo public addressInfo;
+
+    struct LoanAgentInfo {
+        uint256 nodeOwnerStakes;
+        uint256 usersStakes;
+        uint256 poolExpectedExpiry;
+        uint256 rawBytePower;
+        uint256 poolCreationTimestamp;
+        uint24 interestRate;
+        // Amount that the miner has to return (usersStake + interestRateForPeriod)
+        uint256 expectedReturnAmount;
+        uint24 nodeOwnerCollateralStakes;
+    }
+
+    // Amount that the miner has returned to protocol till now
+    /// updated by withdrawBalance function
+    // if totalReturnedAmount == expectedReturnAmount (changeOwner of the miner and terminate the stakepool)
+    uint256 totalReturnedAmount;
+
+    LoanAgentInfo public loanAgentInfo;
+
+    /// checkpoint => withdrawnAmount
+    mapping(uint256 => uint256) withdrawHistory;
+    /// checkpoint => available balance after withdrawl
+    mapping(uint256 => uint256) availableBalanceHistory;
+    // checkpoint => miner share
+    mapping(uint256 => uint256) ownerWithrawableAmount;
+    // total amount of miner share accummulated
+    uint256 totalOwnerCredit;
+    uint256 lastWithdrawlCheckpoint;
+
+    enum NodeStatus {
+        InQueue,
+        WaitingForOwnerChange,
+        WaitingForOracleVerifiction,
+        Active,
+        Paused,
+        Terminated
+    }
+
+    NodeStatus public status;
+
+    // Stakepool factory
+    address public immutable factory;
 
     // should be called using LoanAgentFactory
     constructor(
-        address _lendingVaultAddress,
-        address _minerAddress,
-        address _oracleAddress,
-        address workerAddress
+        AddressInfo memory _addressInfo,
+        LoanAgentInfo memory _loanAgentInfo
     ) {
         // Call ChangeWorkerAddress in MinerAPI: https://docs.zondax.ch/fevm/filecoin-solidity/api/actors/Miner#changeworkeraddress
     }
@@ -40,9 +67,55 @@ contract LoadAgent {
     // transfer the reward from this contract to LendingVault, Miner, Oracle, respectively
     function distributeReward() external {}
 
-    // Only called by minder address
+    // Only called by miner address
     function changeWorker(address newWorkerAddress) external {}
 
-    // Only called by LendingVault when the loan is fully repaid
-    function changeOwner(address ownerAddress) external {}
+    // miner can repay the loan and change the owner to itself, LoanAgent will selfdestruct and transfer the funds to LendingVault
+    function repayLoanAndChangeOwner(address ownerAddress) external {}
+
+    function changeOwnerBackToReal() external;
+
+    function getBalanceInfo()
+        external
+        view
+        returns (
+            uint256 availableBalance,
+            uint256 lockedReward,
+            uint256 initialPledgeCollateral
+        );
+
+    function getStakeInfo()
+        external
+        view
+        returns (uint256 nodeOnwerStakes, uint256 usersStakes);
+
+    function getNodeOwner() external view returns (address);
+
+    // Can only be callled by oracle contract or stakepool factory
+    function updateNodeStatus(NodeStatus status) external;
+
+    function destructStakePool() external onlyFactory {}
+
+    // Can only be called by oracle contract and before status == NodeStatus.Active
+    // get pledged collateral, calculate residual FIL in the wallet after pledging,
+    // refund the amounts according to the collateral shares and update nodeOwnerStakes and usersStakes
+    function verifyAndRefundUnpledgedCollateral() external;
+
+    // Withdraw earnings from the available balance
+    // Only the oracle smart contract can call this function
+    // if timeCommitment is crossed and expectedTotal return is yet not procured, 80% of minerShare will also be added in usersShare
+    // only withdraw if amount <= current available balance - availableBalanceHistory[lastWithdrawlCheckpoint];
+    // The minerShare won't be withdrawn and the amount will be added in totalMinerCredit which the miner can withdraw anytime
+    // update availableBalanceHistory, withdrawHistory and lastWithdrawlCheckpoint
+    function withdrawEarnings(
+        uint256 checkpoint,
+        uint256 minerShare,
+        uint256 usersShare,
+        uint256 oracleShare
+    ) external onlyOracleContract {}
+
+    // The amount that realOwner can withdraw anytime.
+    // The amount must be less than totalOwnerCredit and available balance.
+    // The withdrawal will update totalOwnerCredit, availableBalanceHistory
+    function withdrawMinerEarning(uint256 amount) external onlyRealOwner {}
 }
--- src/LoanAgentFactory.sol
+++ src/LoanAgentFactory.sol
@@ -2,7 +2,34 @@
 pragma solidity ^0.8.13;
 
 contract LoadAgentFactory {
+    enum LoanRequestStatus{ PENDING, APPROVED, REJECTED }
+    mapping(address => bool) registeredLoanAgents;
+
+    modifier onlyOracle {
+        _;
+    }
+
+    struct LoanRequest {
+        address miner;
+        address owner;
+        address requestOwner;
+        uint256 rawBytesPower;
+        // miner will transfer 10% extra amount for rate changes
+        // which will be refunded after sector pledging if not used
+        uint256 pledgeAmount;
+        uint256 timeCommitement;
+        uint256 requestCreationTimestamp;
+        LoanRequestStatus status;
+    }
+
+    LoanRequest[] loanRequestQueue;
     // called by Oracle contract can call this, transfer the loan to the LoanAgent contract
     // apply CREATE2 so the contract address can be known beforehand
     function deployLoanAgent() external {}
+
+    function addLoanRequest(LoanRequest calldata request) external payable {}
+
+    // Can be called either by oracle contract or request owner.
+    function removeLoanRequest(address agent) external {}
+    function registerNewAgents(address[] calldata agents) external onlyOracle {}
 }
