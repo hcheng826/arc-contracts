410469bda6d6bbfc5108fe6993a62241ffb128ea
hcheng826 <lilioo826@gmail.com>
2023-03-17 09:01:17 +0800

    deposit and withdraw

--- src/aFIL.sol
+++ src/aFIL.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/utils/math/Math.sol";
 import "./LoanAgent.sol";
 
-// is ERC20
-contract aFIL {
+error InsufficientTokenAvailable(uint availableAmount, uint requestAmount);
+
+contract aFIL is ERC20 {
+    uint totalLoanAmount;
+
+    constructor() ERC20("arc FIL", "aFIL") {}
+
     modifier onlyOracle() {
         _;
     }
@@ -16,10 +23,42 @@ contract aFIL {
         uint remainingAmount;
     }
 
-    function depositFIL() public payable {}
+    function depositFIL() public payable {
+        _mint(msg.sender, getAfilValue(msg.value));
+    }
 
-    function withdrawFIL() public {}
+    function withdrawFIL(uint amount) public {
+        if (amount > balanceOf(msg.sender)) {
+            revert InsufficientTokenAvailable(balanceOf(msg.sender), amount);
+        }
+        _burn(msg.sender, amount);
+        payable(msg.sender).transfer(getFilValue(amount));
+    }
+
+    // Calculate the amount of aFIL backed by an amount of FIL
+    // ref: https://github.com/rocket-pool/rocketpool/blob/967e4d3c32721a84694921751920af313d1467af/contracts/contract/token/RocketTokenRETH.sol#L51
+    function getAfilValue(uint filAmount) public view returns (uint256) {
+        uint afilSupply = totalSupply();
+        if (afilSupply == 0) {
+            return filAmount;
+        }
+        return (filAmount * afilSupply) / getBackingFilBalance();
+    }
+
+    // Calculate the amount of FIL backing an amount of aFIL
+    // ref: https://github.com/rocket-pool/rocketpool/blob/967e4d3c32721a84694921751920af313d1467af/contracts/contract/token/RocketTokenRETH.sol#LL39C68-L39C87
+    function getFilValue(uint afilAmount) public view returns (uint256) {
+        uint afilSupply = totalSupply();
+        if (afilSupply == 0) {
+            return afilAmount;
+        }
+        return (afilAmount * getBackingFilBalance()) / afilSupply;
+    }
+
+    function getBackingFilBalance() public view returns (uint256) {
+        return address(this).balance + totalLoanAmount;
+    }
 
     // transfer FIL to loanAgent and record the loan data
-    function loan(address loanAgent, uint amount) external onlyOracle {}
+    function loan(address payable loanAgent, uint amount) external onlyOracle {}
 }
